7cb901c52c7a87e9be586941da0e9e62791edb22
ken343 <kmcdole21@gmail.com>
2020-03-24 10:21:19 +0000

    Commented out set -e because go get always errors

--- cicd/build_server.sh
+++ cicd/build_server.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # Exit immediately if a command exits with a non-zero status
-set -e
+# set -e
 
 # This path should be GOPATH=/go/src/github.com/.... when using the golang container
 export GOPATH=$(pwd)/go
